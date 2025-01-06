d16cdb492da8737f28d74f4cb4d6ea0ee07d281a
Robert Magnusson <robert@northmind.io>
2025-01-06 14:21:35 +0100

    fix broken example app pckage.json

--- Examples/ExampleApp/Package.swift
+++ Examples/ExampleApp/Package.swift
@@ -10,7 +10,8 @@ let package = Package(
     targets: [
         .executableTarget(
             name: "ExampleApp",
-            dependencies: [.product(name: "SwiftPod", package: "SwiftPod")]
+            dependencies: [.product(name: "SwiftPod", package: "SwiftPod")],
+            path: "Sources/"
         ),
     ]
 )
